a9584be484c31eac94ff53b399c041adb9143eaf
maant001 <mamaril_a@me.com>
2023-05-01 19:31:28 +0200

    Initial Commit

---
+++ MOBA2_Calculator/MOBA2_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOBA2_CalculatorApp.swift
+//  MOBA2_Calculator
+//
+//  Created by Tony Mamaril on 01.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MOBA2_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
